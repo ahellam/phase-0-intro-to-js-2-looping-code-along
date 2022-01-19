3c6bba6b176a71ad8d8355bcb8895a6923c25865
Aaron Hellam <aaronhellam@gmail.com>
2022-01-19 12:59:21 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const arr1 =[]
+function writeCards(names, event){
+  let arr2 = [...arr1];
+    for (let i = 0; i < names.length; i++) {
+        arr2.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        // debugger;
+      }
+      return arr2;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise");
+
+function countDown(num){
+    let number = num;
+    while (number >= 0){
+        console.log(number);
+        number --;
+    }
+}
